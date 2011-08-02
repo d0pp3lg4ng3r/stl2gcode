81f082d33fdf745a4761cfbcfb838e5e174f1069
d0pp3lg4ng3r <rfredette27@gmail.com>
2011-08-02 15:28:20 -0400

    redesigned fill-line algorithm

--- stl_gcode.pl
+++ stl_gcode.pl
@@ -14,40 +14,31 @@ my @facets;
 my @loops;
 
 #foreach z layer:
-for(my $i = $zmin; $i < $zmax; $i += $offset) {
-    my @lines;
-#   foreach facet:
-#       if facet is in current z layer:
-#           get intersect line, and add it to the list of lines in this layer
-#   take first line
-    my $start_line = shift @lines;
-    my @current_loop = ($start_line);
-    push @loops, \@current_loop;
-    my $line_found = 0;
-#   find the line whose beginning point is within acceptable distance from the current point
-    do {
-        for (my $i = 0; $i < scalar @lines; $i++) {
-            my $line = $lines[$i];
-            if (abs($start_line->{x} - $line->{x}) < $offset) {
-                if (abs($start_line->{y} - $line->{y}) < $offset) {
-                    push @current_loop, $line;
-                    splice @lines, $i, 1; #delete 1 item at offset $i from @lines (remove $lines[$i])
-                    $start_line = $line;
-                    $line_found = 1;
-                    last;
-                }
+for (my $z = $zmin; $z < $zmax; $z += $offset) {
+    my $loops = make_loops($z);
+    my $fill_lines = make_fill_lines($z);
+    export($loops, $fill_lines);
+}
+
+sub make_fill_lines {
+    my $z = shift;
+    my $fill_lines = [];
+    for (my $x = $xmin; $x <= $xmax, $x += $offset) {
+        my @points;
+        foreach my @loop (@loops) {
+            foreach my @line (@loop) {
+#               $x must be less than one point but greater than the other. if it is less than/greater than BOTH points, skip it
+                next if ($x < $line[0]{x} && $x < $line[1]{x});
+                next if ($x > $line[0]{x} && $x > $line[1]{x});
+                my $slope = ($line[1]{y} - $line[0]{y})/($line[1]{x} - $line[0]{x});
+                my $y = $slope * ($x - $line[0]{x}) + $line[0]{y};
+                push @points, {x => $x, y => $y};
             }
         }
-        if (!$line_found) {
-#   if the beginning of the first line and the end of the last point don't match, die due to incomplete structure
-            die "bad loop\n$!" if(abs($current_loop[0]->{x} - $current_loop[-1]->{x}) > $offset);
-            die "bad loop\n$!" if(abs($current_loop[0]->{y} - $current_loop[-1]->{y}) > $offset);
-#   if there are still unordered lines, repeat from 'take first line'
-            $start_line = shift @lines;
-            @current_loop = ($start_line);
-            push @loops, \@current_loop;
+        @points = sort {$a->{y} <=> $b->{y}} @points;
+        for ($i = 0; $i < @points; $i++) {
+            push @{$fill_lines->[$i]}, $points[$i];
         }
-    } while (scalar @lines > 0);
-        
-#   repeat previous line until no lines can be found
+    }
+    return $fill_lines;
 }
